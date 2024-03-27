9f486af882a91c3112681d3ca6304f9f1a628a53
Tongyu <tongyu.yuan@datadoghq.com>
2024-03-27 19:03:49 +0100

    Rebuild react app with ts, add pageLogin draft

--- backend/api/go.mod
+++ backend/api/go.mod
@@ -1,4 +1,4 @@
-module api
+module api 
 
 go 1.22
 
--- frontend/app/src/index.tsx
+++ frontend/app/src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { PageLogin } from './components/login/PageLogin';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <PageLogin />
   </React.StrictMode>
 );
 
